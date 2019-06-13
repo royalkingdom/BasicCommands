ff30b09a036791bbaab53d76ac3d355d51d7fbf1
Ayley <larshatzky@gmail.com>
2019-06-13 19:48:08 +0200

    Add some Messages for Day Command

--- src/main/java/de/tlph1/util/ConfigWerte.java
+++ src/main/java/de/tlph1/util/ConfigWerte.java
@@ -1,8 +1,5 @@
 package de.tlph1.util;
 
-import de.tlph1.commands.*;
-import de.tlph1.util.Join;
-import de.tlph1.util.Quit;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -21,6 +18,17 @@ public class ConfigWerte {
     public final String Prefix = cfg.getString("Settings.Prefix");
     public final String PluginStart = cfg.getString("Settings.PluginStart");
     public final String NoPerm = cfg.getString("Settings.NoPerm");
+    public final String PlayerCommand = cfg.getString("Settings.PlayerCommand");
+
+    //JoinLeave
+    public final String JoinMessage = cfg.getString("BasicCommnds.JoinLeave.Join");
+    public final String QuitMessage = cfg.getString("BasicCommnds.JoinLeave.Quit");
+
+    //DayNight
+    public final String Day = cfg.getString("BasicCommands.DayNight.Day");
+    public final String Night = cfg.getString("BasicCommands.DayNight.Night");
+    public final String DayCMD = cfg.getString("BasicCommands.DayNight.DayCMD");
+    public final String NightCMD = cfg.getString("BasicCommands.DayNight.NightCMD");
 
     public static void playerMessage(Player player, String message){
         player.sendMessage(ChatColor.translateAlternateColorCodes('&',message));
--- src/main/java/de/tlph1/util/MessageConfig.java
+++ src/main/java/de/tlph1/util/MessageConfig.java
@@ -4,6 +4,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MessageConfig {
 
@@ -11,7 +12,26 @@ public class MessageConfig {
     private FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
 
     public void setMessages(){
-
+        if(!cfg.contains("BasicCommands.JoinLeave")){
+            cfg.set("BasicCommnds.JoinLeave.Join", "&e%player% &aist dem Königreich beigetreten");
+            cfg.set("BasicCommnds.JoinLeave.Quit", "&e%player% &chat das Königreich verlassen");
+        }
+        if(!cfg.contains("Settings")){
+            cfg.set("Settings.PlayerCommand", "Dieser Befehl darf man nur als Spieler ausführen !");
+        }
+        if(!cfg.contains("BasicCommands.DayNight")){
+            cfg.set("BasicCommands.DayNight.Day", "&2Es ist nun Tag!");
+            cfg.set("BasicCommands.DayNight.DayCMD", "&2Benutze /day!");
+            cfg.set("BasicCommands.DayNight.Night", "&2Es ist nun Nacht!");
+            cfg.set("BasicCommands.DayNight.NightCMD", "&2Benutze /night!");
+        }
+        save();
     }
-
+    private void save(){
+        try {
+            cfg.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
